b55358425ca1956616fc0351b959810c72f37574
Денис <karasov-dv@ural.rt.ru>
2022-10-23 19:00:17 +0500

    Добавил обработку исключений, немного отрефакторил

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,57 +3,80 @@ package src.main;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
-    public static void main(String args[]) throws IOException {
-        System.out.println("Введите первое число:");
-        int one = inputInt();
-        System.out.println("Введите знак математической операции (+, -, /, *):");
-        String operator = inputOperator();
-        System.out.println("Введите второе число:");
-        int two = inputInt();
-        if (operator.equals("+"))
-            plus(one, two);
-        else if (operator.equals("-"))
-            minus(one, two);
-        else if (operator.equals("*"))
-            multiplication(one, two);
-        else if (operator.equals("/"))
-        division(one, two);
+    public static void main(String[] args) throws IOException {
+        while (true) {
+            System.out.println("Введите первое число:");
+            double one = inputInt();
+            System.out.println("Введите знак математической операции (+, -, /, *):");
+            String operator = inputOperator();
+            System.out.println("Введите второе число:");
+            double two = inputInt();
+            switch (operator) {
+                case "+" -> plus(one, two);
+                case "-" -> minus(one, two);
+                case "*" -> multiplication(one, two);
+                case "/" -> division(one, two);
+            }
+        }
 
     }
 
-    public static int inputInt() throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int data = Integer.parseInt(reader.readLine());
+    public static Double inputInt() throws IOException {
+        double data;
+        while (true) {
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                data = Double.parseDouble(reader.readLine());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Вы ввели не число. Повторите ввод:");
+            }
+        }
         return data;
     }
 
+
     public static String inputOperator() throws IOException {
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String operator = reader.readLine();
+        String operator;
+        while (true) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            operator = reader.readLine();
+            List<String> data = new ArrayList<>();
+            data.add("+");
+            data.add("-");
+            data.add("*");
+            data.add("/");
+            if (data.contains(operator))
+                break;
+            else
+                System.out.println("Введенный символ не является знаком. Повторите ввод:");
+        }
         return operator;
-    }
-
-    public void comparerOperator() {
 
     }
-    public static void plus(int one, int two) {
-        int result = one + two;
+
+    public static void plus(double one, double two) {
+        double result = one + two;
         System.out.println("Ответ: " + result);
     }
-    public static void minus(int one, int two) {
-        int result = one - two;
+
+    public static void minus(double one, double two) {
+        double result = one - two;
         System.out.println("Ответ: " + result);
     }
-    public static void division(int one, int two) {
-        int result = one / two;
+
+    public static void division(double one, double two) {
+        double result = one / two;
         System.out.println("Ответ: " + result);
     }
-    public static void multiplication(int one, int two) {
-        int result = one * two;
+
+    public static void multiplication(double one, double two) {
+        double result = one * two;
         System.out.println("Ответ: " + result);
     }
 }
